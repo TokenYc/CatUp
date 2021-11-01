6aeddd6048bc6345d56547cbe72a4ca922cc5be1
mayouli <247067345@qq.com>
2021-11-01 17:31:58 +0800

    compose环境搭建完成

--- app/src/main/java/com/qianfan/catup/MainActivity.kt
+++ app/src/main/java/com/qianfan/catup/MainActivity.kt
@@ -9,56 +9,60 @@ import android.provider.Settings
 import android.text.TextUtils
 import android.text.TextUtils.SimpleStringSplitter
 import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.Text
 import androidx.core.text.isDigitsOnly
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        updateKeyword()
-        updateViewTime()
-        btn_open_permission.setOnClickListener {
-            if (!isAccessibilitySettingsOn(this, CatService::class.java)) {
-                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
-            }
+        setContent {
+            Text(text = "hello world!")
         }
-        btn_save_keyword.setOnClickListener {
-            val keyword = et_keyword.text.toString()
-            if (!TextUtils.isEmpty(keyword)) {
-                Params.keyword = keyword
-                updateKeyword()
-                Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "请输入关键词", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        btn_save_view_time.setOnClickListener {
-            val viewTime = et_view_time.text.toString()
-            if (!TextUtils.isEmpty(viewTime) && viewTime.isDigitsOnly()) {
-                Params.viewTime = viewTime.toLong()
-                updateViewTime()
-                Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "请输入正确格式的浏览时间", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        setContentView(R.layout.activity_main)
+//        updateKeyword()
+//        updateViewTime()
+//        btn_open_permission.setOnClickListener {
+//            if (!isAccessibilitySettingsOn(this, CatService::class.java)) {
+//                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+//            }
+//        }
+//        btn_save_keyword.setOnClickListener {
+//            val keyword = et_keyword.text.toString()
+//            if (!TextUtils.isEmpty(keyword)) {
+//                Params.keyword = keyword
+//                updateKeyword()
+//                Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "请输入关键词", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//
+//        btn_save_view_time.setOnClickListener {
+//            val viewTime = et_view_time.text.toString()
+//            if (!TextUtils.isEmpty(viewTime) && viewTime.isDigitsOnly()) {
+//                Params.viewTime = viewTime.toLong()
+//                updateViewTime()
+//                Toast.makeText(this, "设置成功", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this, "请输入正确格式的浏览时间", Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
-    @SuppressLint("SetTextI18n")
-    fun updateKeyword() {
-        tv_keyword.text = "当前点击关键词：" + Params.keyword
-    }
-
-    @SuppressLint("SetTextI18n")
-    fun updateViewTime() {
-        tv_view_time.text = "当前页面浏览时间：" + Params.viewTime + "秒"
-    }
-
-
+//    @SuppressLint("SetTextI18n")
+//    fun updateKeyword() {
+//        tv_keyword.text = "当前点击关键词：" + Params.keyword
+//    }
+//
+//    @SuppressLint("SetTextI18n")
+//    fun updateViewTime() {
+//        tv_view_time.text = "当前页面浏览时间：" + Params.viewTime + "秒"
+//    }
+//
+//
     fun isAccessibilitySettingsOn(
         mContext: Context,
         clazz: Class<out AccessibilityService?>
